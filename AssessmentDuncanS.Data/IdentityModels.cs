89b6dc19387ad7b842402f942914e38ca9aa4a8e
Duncan Summers <djsummers36@gmail.com>
2022-01-19 22:17:44 -0500

    added Message data class, changed connection string in webconfig

--- AssessmentDuncanS.Data/IdentityModels.cs
+++ AssessmentDuncanS.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -31,6 +33,38 @@ namespace AssessmentDuncanS.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        // Set up database for messages
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    
+    // Configure user logins and roles
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
---
+++ AssessmentDuncanS.Data/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentDuncanS.Data
+{
+    // set up properties of messages in data layer
+    public class Message
+    {
+        [Key]
+        public int MessageID { get; set; }
+
+        [Required]
+        public Guid Sender { get; set; }
+
+        [Required]
+        public Guid Recipient { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage ="Please type your message")]
+        [MaxLength(1000, ErrorMessage ="Message exceeds character limit")]
+        public string Content { get; set; }
+    }
+}
